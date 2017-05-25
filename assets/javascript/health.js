af799b240d567be01f308b414e8fdb9f26a4df62
Ebru Yucesar <ebru.yucesar@gmail.com>
2017-05-25 18:04:52 -0500

    added form validation to first calculator

--- assets/javascript/health.js
+++ assets/javascript/health.js
@@ -2,22 +2,28 @@ $(document).ready(function () {
 
 	//BMI calculator
 	$("#bmiSubmit").on("click", function(event) {
-		event.preventDefault();
-		//grab the value from the input field and convert it into an integer
-		var bmiWeight = parseInt($("#bmiWeight").val().trim());
-		var bmiHeight = parseInt($("#bmiHeight").val().trim());
-		//convert the height into meters
-		bmiHeight = bmiHeight / 100;
-		//the logic for the calculation
-		var bmi = bmiWeight / (bmiHeight * bmiHeight);
-		//testing
-		console.log(bmi);
-		//end up only with one decimal
-		bmi = Math.round(bmi * 10) / 10;
-		//write the result to the screen
-		$("#bmiResult").val(bmi);
-		//testing
-		console.log(bmi);
+		//adding the if statement for form validation. The number will be calculated only if an input was entered by the user.
+		if ($("#bmiWeight").val().length > 0 && $("#bmiHeight").val().length > 0) {
+				event.preventDefault();
+				//grab the value from the input field and convert it into an integer
+				var bmiWeight = parseInt($("#bmiWeight").val().trim());
+				var bmiHeight = parseInt($("#bmiHeight").val().trim());
+				//convert the height into meters
+				bmiHeight = bmiHeight / 100;
+				//the logic for the calculation
+				var bmi = bmiWeight / (bmiHeight * bmiHeight);
+				//testing
+				console.log(bmi);
+				//end up only with one decimal
+				bmi = Math.round(bmi * 10) / 10;
+				//write the result to the screen
+				$("#bmiResult").val(bmi);
+				//testing
+				console.log(bmi);
+		} else {
+			$("#bmiWeight").css("border", "1px solid red");
+			$("#bmiHeight").css("border", "1px solid red");
+		}
 	});
 
 	//Lean Body Mass Kg Calculator
