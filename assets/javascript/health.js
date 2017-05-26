bc7e77f8612b845fea673c3a732906e505308fb3
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-05-26 00:29:30 -0500

    added JS to 2 of the calculators

--- assets/javascript/health.js
+++ assets/javascript/health.js
@@ -28,6 +28,45 @@ $(document).ready(function () {
 
 	// Body Fat % Calculator
 	// -----------------------------------------------
+	$("#bodyFatSubmit").on("click", function() {
+		// Operate only if all inputs are added
+		if ($("#bodyFatWeight").val().length > 0 && $("#waistCir").val().length > 0 && $("#wristCir").val().length > 0 && $("#hipCir").val().length > 0 && $("#forearm-cir").val().length > 0) {
+			event.preventDefault();
+			// Grabbing values from input boxes
+			var bodyFatWeight = parseInt($("#bodyFatWeight").val().trim());
+			var waistCir	  = parseInt($("#waistCir").val().trim());
+			var wristCir	  = parseInt($("#wristCir").val().trim());
+			var hipCir		  = parseInt($("#hipCir").val().trim());
+			var forearmCir    = parseInt($("#forearm-cir").val().trim());
+			// Factors
+			bodyFatWeight = (bodyFatWeight * 0.732) + 8.897;
+			wristCir	  = wristCir / 3.140;
+			waistCir	  = waistCir * 0.157;
+			hipCir		  = hipCir * 0.249;
+			forearmCir 	  = forearmCir * 0.434;
+
+			console.log(bodyFatWeight);
+			console.log(waistCir);
+			console.log(wristCir);
+			console.log(hipCir);
+			console.log(forearmCir);
+
+			// 
+			var bodyMass = bodyFatWeight + wristCir + waistCir + hipCir + forearmCir;
+			//
+			var bodyFat  = bodyFatWeight - bodyMass;
+			//
+			var bodyFatPercentage = (bodyFat * 100) / bodyFatWeight;
+			// Reflect Result
+			$("#bodyFatResult").val(bodyFatPercentage); 
+		} else {
+			$("#bodyFatWeight").css("border", "1px solid red");
+			$("#waistCir").css("border", "1px solid red");
+			$("#wristCir").css("border", "1px solid red");
+			$("#hipCir").css("border", "1px solid red");
+			$("#forearm-cir").css("border", "1px solid red");
+		}
+	});
 
 	//Lean Body Mass Kg Calculator
 	$("#leanMassSubmit").on("click", function(event) {
@@ -66,6 +105,46 @@ $(document).ready(function () {
 
 	// Daily Caloric Intake Calculator
 	// ----------------------------------------------
+	// The Harris-Benedict equation is one of the most popular equations for determining energy 
+	// needs by nutrition and health professionals. This equation takes into account a persons 
+	// sex, age, height, weight and level of physical activity. 
+	// The equation for males is: 66.5 + 13.8 x (Weight in kg) + 5 x (Height in cm); 6.8 x age. 
+	// The equation for females is: 655.1 + 9.6 x (Weight in kg) + 1.9 x (Height in cm); 4.7 x age. 
+	// These equations are then multiplied by an energy factor to determining estimated caloric needs. 
+	// The physical activity factors are 1.2 for sedentary people, 1.3 for moderately active people and 1.4 for active people.
+
+	$("#caloricIntakeSubmit").on("click", function(){
+		// Grabbing variables
+		var caloricAge 		= $("#caloricAge").val().trim();
+		var caloricWeight 	= $("#caloricWeight").val().trim();
+		var caloricHeight 	= $("#caloricHeight").val().trim();
+		var caloricExercise = $("#caloricExercise").val(); // Does not need trim since there is already a value from the HTML
+
+		console.log(caloricAge);
+		console.log(caloricWeight);
+		console.log(caloricHeight);
+		console.log(caloricExercise);
+
+		// If female is selected
+		if ($("#sexFem").attr("checked")) {
+		// Equation
+		var caloricIntake = 655.1 + (9.6 * caloricWeight) + (1.9 * caloricHeight);
+		caloricAge 		  = (4.7 * caloricAge);
+		$("#caloricIntakeResult").val(caloricIntake + "calories");
+		} 
+		  // If male is selected
+		  else if ($("#sexMale").attr("checked")) {
+		  var caloricIntake = 66.5 + (13.8 * caloricWeight) + (5 * caloricHeight);
+		  caloricAge 		= (6.8 * caloricAge);
+		  $("#caloricIntakeResult").val(caloricIntake + "calories");
+
+		} else {
+		  $("#caloricAge").css("border", "1px solid red");
+		  $("#caloricWeight").css("border", "1px solid red");
+		  $("#caloricHeight").css("border", "1px solid red");
+		  $("#caloricExercise").css("border", "1px solid red");	
+		}
+	});
 
 
 	// Protein RDA Calculator
