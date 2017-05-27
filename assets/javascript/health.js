6871311db9b7dbada1ca98fcc040ec752041d00d
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-05-27 13:43:22 -0500

    JS for calculators nearing complete and fixed a little of the health.html

--- assets/javascript/health.js
+++ assets/javascript/health.js
@@ -27,36 +27,40 @@ $(document).ready(function () {
 	});
 
 	// Body Fat % Calculator
+	// Formula from: http://www.bmi-calculator.net/body-fat-calculator/body-fat-formula.php
 	// -----------------------------------------------
 	$("#bodyFatSubmit").on("click", function() {
 		// Operate only if all inputs are added
 		if ($("#bodyFatWeight").val().length > 0 && $("#waistCir").val().length > 0 && $("#wristCir").val().length > 0 && $("#hipCir").val().length > 0 && $("#forearm-cir").val().length > 0) {
 			event.preventDefault();
-			// Grabbing values from input boxes
+			// Grabbing (metric) values from input boxes
 			var bodyFatWeight = parseInt($("#bodyFatWeight").val().trim());
 			var waistCir	  = parseInt($("#waistCir").val().trim());
 			var wristCir	  = parseInt($("#wristCir").val().trim());
 			var hipCir		  = parseInt($("#hipCir").val().trim());
 			var forearmCir    = parseInt($("#forearm-cir").val().trim());
-			// Factors
-			bodyFatWeight = (bodyFatWeight * 0.732) + 8.897;
+
+			// Converting variable values from metric to imperial since our formula is imperial (I could not find one in metric)
+			bodyFatWeight = bodyFatWeight * 2.20462262;
+			waistCir 	  = waistCir * 0.39370079;
+			wristCir 	  = wristCir * 0.39370079;
+			hipCir 		  = hipCir * 0.39370079;
+			forearmCir 	  = forearmCir * 0.39370079;
+
+			// Converting variables into factors for the body fat formula
+			// We have a new weight variable because we will need to grab the original weight input later
+			var weight 	  = (bodyFatWeight * 0.732) + 8.897;
 			wristCir	  = wristCir / 3.140;
 			waistCir	  = waistCir * 0.157;
 			hipCir		  = hipCir * 0.249;
 			forearmCir 	  = forearmCir * 0.434;
 
-			console.log(bodyFatWeight);
-			console.log(waistCir);
-			console.log(wristCir);
-			console.log(hipCir);
-			console.log(forearmCir);
-
-			// 
-			var bodyMass = bodyFatWeight + wristCir + waistCir + hipCir + forearmCir;
-			//
+			// Lean Body Mass
+			var bodyMass = weight + wristCir - waistCir - hipCir + forearmCir;
+			// Body Fat Weight
 			var bodyFat  = bodyFatWeight - bodyMass;
-			//
-			var bodyFatPercentage = (bodyFat * 100) / bodyFatWeight;
+			// Body Fat Percentage
+			var bodyFatPercentage = (Math.round((bodyFat * 100) / bodyFatWeight) + "%");
 			// Reflect Result
 			$("#bodyFatResult").val(bodyFatPercentage); 
 		} else {
@@ -105,49 +109,65 @@ $(document).ready(function () {
 
 	// Daily Caloric Intake Calculator
 	// ----------------------------------------------
-	// The Harris-Benedict equation is one of the most popular equations for determining energy 
-	// needs by nutrition and health professionals. This equation takes into account a persons 
-	// sex, age, height, weight and level of physical activity. 
-	// The equation for males is: 66.5 + 13.8 x (Weight in kg) + 5 x (Height in cm); 6.8 x age. 
-	// The equation for females is: 655.1 + 9.6 x (Weight in kg) + 1.9 x (Height in cm); 4.7 x age. 
-	// These equations are then multiplied by an energy factor to determining estimated caloric needs. 
+	// Mifflin-St. Jeor Equation - http://www.livestrong.com/article/178764-caloric-intake-formula/ 
 	// The physical activity factors are 1.2 for sedentary people, 1.3 for moderately active people and 1.4 for active people.
 
 	$("#caloricIntakeSubmit").on("click", function(){
+		//if ($("#caloricAge").val().length > 0 && $("#caloricWeight").val().length > 0 && $("#caloricHeight").val().lenght > 0) {
+		event.preventDefault();
 		// Grabbing variables
-		var caloricAge 		= $("#caloricAge").val().trim();
-		var caloricWeight 	= $("#caloricWeight").val().trim();
-		var caloricHeight 	= $("#caloricHeight").val().trim();
-		var caloricExercise = $("#caloricExercise").val(); // Does not need trim since there is already a value from the HTML
+		var caloricAge 		= parseInt($("#caloricAge").val().trim());
+		var caloricWeight 	= parseInt($("#caloricWeight").val().trim());
+		var caloricHeight 	= parseInt($("#caloricHeight").val().trim());
+		var caloricExercise = parseInt($("#caloricExercise").val()); // Does not need trim since there is already a value from the HTML
 
 		console.log(caloricAge);
 		console.log(caloricWeight);
 		console.log(caloricHeight);
 		console.log(caloricExercise);
 
-		// If female is selected
-		if ($("#sexFem").attr("checked")) {
-		// Equation
-		var caloricIntake = 655.1 + (9.6 * caloricWeight) + (1.9 * caloricHeight);
-		caloricAge 		  = (4.7 * caloricAge);
-		$("#caloricIntakeResult").val(caloricIntake + "calories");
-		} 
-		  // If male is selected
-		  else if ($("#sexMale").attr("checked")) {
-		  var caloricIntake = 66.5 + (13.8 * caloricWeight) + (5 * caloricHeight);
-		  caloricAge 		= (6.8 * caloricAge);
-		  $("#caloricIntakeResult").val(caloricIntake + "calories");
+		var caloricIntake = (10 * caloricWeight) + (6.25 * caloricHeight) - (5 * caloricAge) - 161;
+		caloricIntake 	  = caloricIntake * caloricExercise;
+		$("#caloricIntakeResult").val(caloricIntake + " calories");
 
-		} else {
-		  $("#caloricAge").css("border", "1px solid red");
-		  $("#caloricWeight").css("border", "1px solid red");
-		  $("#caloricHeight").css("border", "1px solid red");
-		  $("#caloricExercise").css("border", "1px solid red");	
-		}
-	});
+		// // If female is selected
+		// if (document.getElementById("sexFem").checked) {
+		// // Equation
+		// var caloricIntake = (10 * caloricWeight) + (6.25 * caloricHeight) - (5 * caloricAge) - 161;
+		// caloricIntake 	  = caloricIntake * caloricExercise;
+		// $("#caloricIntakeResult").val(caloricIntake + "calories");
+		// } 
+		//   // If male is selected
+		//   else if (document.getElementById("sexMale").checked) {
+		//   var caloricIntake = (10 * caloricWeight) + (6.25 * caloricHeight) - (5 * caloricAge) + 5;
+		//   caloricIntake 	= caloricIntake * caloricExercise;
+		//   $("#caloricIntakeResult").val(caloricIntake + "calories");
+
+		// } else {
+		//   $("#caloricAge").css("border", "1px solid red");
+		//   $("#caloricWeight").css("border", "1px solid red");
+		//   $("#caloricHeight").css("border", "1px solid red");
+		//   $("#caloricExercise").css("border", "1px solid red");	
+		// }
+	//}
+});
 
 
 	// Protein RDA Calculator
 	// ----------------------------------------------
+	$("#proteinSubmit").on("click", function() {
+		event.preventDefault();
+		// Grabbing variables
+		var proteinAge 	  = parseInt($("#proteinAge").val().trim());
+		var proteinWeight = parseInt($("#proteinWeight").val().trim());
+		var proteinHeight = parseInt($("#proteinHeight").val().trim());
+		var proteinExercise = parseInt($("#proteinExercise").val());
 
+		console.log(proteinAge);
+		console.log(proteinWeight);
+		console.log(proteinHeight);
+
+		var proteinRDA = Math.round(proteinWeight * 0.8);
+		$("#proteinResult").val(proteinRDA + " g");
+	})
 });
