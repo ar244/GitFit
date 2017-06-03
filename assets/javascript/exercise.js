7488c3794e9e4ca069a8394d265bf3d93b4fd297
ar244 <anu244@gmail.com>
2017-06-03 12:21:03 -0500

    Separated display exercise function so that it can be called from the muscle diagram selection

--- assets/javascript/exercise.js
+++ assets/javascript/exercise.js
@@ -16,65 +16,75 @@ $(document).ready(function () {
 	var queryURLBefore = "https://wger.de/api/v2/exercise/?key=7984176e785d9a22346cacb2840d8ddb961748d3&language=2&muscles="; 
 	var queryURL = "";
 
-	$("#menu li a").on('click', function(){
-	  event.preventDefault();
-  	$('#muscleMenu').text($(this).text());
+	
+    // Take muscle name and query and display exercises
+    function displayExercise(muscle) {
+      muscleIndex = parseInt(muscleName.indexOf(muscle)) + 1;
 
-  	queryURL = "";
+    	queryURL = queryURLBefore +muscleIndex;
 
-  	$("#exerciseSpace").empty();
+      	
+  		console.log(queryURL);
 
-  	muscle = $(this).text();
-  	muscleIndex = parseInt(muscleName.indexOf(muscle)) + 1;
+  		$.ajax({
+  			crossDomain: false,
+  	    	url: queryURL,
+  	    	method: "GET"
+    		}).done(function(response) {
+    			console.log(response);
+    			
+    			for (var i=0; i<Math.min(10, response.results.length); i++) {
+    				var name = (response.results[i].name);
+    				var des = (response.results[i].description);
+            var secMuscles = response.results[i].muscles_secondary;
+            var equipment = response.results[i].equipment;
+            var idName = "exercise"+(i+1);
 
-  	queryURL = queryURLBefore +muscleIndex;
-
-    	
-		console.log(queryURL);
-
-		$.ajax({
-			crossDomain: false,
-	    	url: queryURL,
-	    	method: "GET"
-  		}).done(function(response) {
-  			console.log(response);
-  			
-  			for (var i=0; i<Math.min(10, response.results.length); i++) {
-  				var name = (response.results[i].name);
-  				var des = (response.results[i].description);
-          var secMuscles = response.results[i].muscles_secondary;
-          var equipment = response.results[i].equipment;
-          var idName = "exercise"+(i+1);
-
-          // Display exercise info 
-          // Below if is needed since some of the exercises have no description
-          if (des) {
-            $("#exerciseSpace").append("<button type='button' class='btn btn-info exercise-name' data-toggle='collapse' data-target='#"+idName+"'>"+name+"</button>");
-            $("#exerciseSpace").append("<br><br>");
-            $("#exerciseSpace").append("<div id="+idName+" class='collapse collapse-div'></div>");
-            
-            $("#"+idName).append("<h5 class='descHeader'>Description</h5><br>");
-            $("#"+idName).append("<p>"+des+"</p><br>");
-            
-            if (secMuscles.length > 0) {
-              $("#"+idName).append("<h5 class='descHeader'>Also works</h5><br>");
-              for (var j=0; j<secMuscles.length; j++) {
-                $("#"+idName).append("<p>"+muscleName[secMuscles[j]-1]+"</p>");
+            // Display exercise info 
+            // Below if is needed since some of the exercises have no description
+            if (des) {
+              $("#exerciseSpace").append("<button type='button' class='btn btn-info exercise-name' data-toggle='collapse' data-target='#"+idName+"'>"+name+"</button>");
+              $("#exerciseSpace").append("<br><br>");
+              $("#exerciseSpace").append("<div id="+idName+" class='collapse collapse-div'></div>");
+              
+              $("#"+idName).append("<h5 class='descHeader'>Description</h5><br>");
+              $("#"+idName).append("<p>"+des+"</p><br>");
+              
+              if (secMuscles.length > 0) {
+                $("#"+idName).append("<h5 class='descHeader'>Also works</h5><br>");
+                for (var j=0; j<secMuscles.length; j++) {
+                  $("#"+idName).append("<p>"+muscleName[secMuscles[j]-1]+"</p>");
+                }
               }
-            }
 
-            if (equipment.length > 0) {
-              $("#"+idName).append("<br><h5 class='descHeader'>Equipment</h5><br>");
-              for (var j=0; j<equipment.length; j++) {
-                $("#"+idName).append("<p>"+equipmentName[equipment[j]-1]+"</p>");
+              if (equipment.length > 0) {
+                $("#"+idName).append("<br><h5 class='descHeader'>Equipment</h5><br>");
+                for (var j=0; j<equipment.length; j++) {
+                  $("#"+idName).append("<p>"+equipmentName[equipment[j]-1]+"</p>");
+                }
               }
-            }
-  			 } // if (des)
-      }
-  			
-  	});
+    			 } // if (des)
+        }
+    			
+    	});
 
-	});
+  	}
+
+  // When MuscleGroup button is clicked
+  $("#menu li a").on('click', function(){
+    event.preventDefault();
+    $('#muscleMenu').text($(this).text());
+
+    queryURL = "";
+
+    $("#exerciseSpace").empty();
+
+    muscle = $(this).text();
+    displayExercise(muscle);
+
+  });
+
+
 
   //toggle button function
   //saves the button itself as a variable and applies three data attribute
